671cf1ada045f462319ea93e52488d24474c76ee
wangyonghao <wyh24370708@163.com>
2020-05-04 18:47:56 +0800

    添加git_test工程

---
+++ git_test/src/com/wyh/test.java
@@ -0,0 +1,14 @@
+package com.wyh;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
